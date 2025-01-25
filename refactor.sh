ba815e25f90447fb2caab16e67c097bcbfc7bb5f
Markus Katharina Brechtel <markus.katharina.brechtel@thengo.net>
2025-01-25 22:38:38 +0100

    fix refactoring script default var handling
    Signed-off-by: Markus Katharina Brechtel <markus.katharina.brechtel@thengo.net>

--- refactor.sh
+++ refactor.sh
@@ -30,7 +30,7 @@ for book in "${books[@]}"; do
     mkdir -p "roles/${book}/tasks" "roles/${book}/vars" "roles/${book}/files" "roles/${book}/templates" "roles/${book}/defaults" "roles/${book}/meta" "roles/${book}/handlers"
 
     # defaults
-    sed -n -e '/^  vars:/,/^  [a-z]/{//!p}' "ansible/books/${book}.yaml" | sed 's/^    //g' | grep 'default(.*)' | awk  '!x[$0]++' | sed -E 's/^([a-z_]*):.*default\((.*)\).*$/\1: \2/g' | sed -e '1i---' -e "s/task_/${book}_/g" > "roles/${book}/defaults/main.yaml"
+    sed -n -e '/^  vars:/,/^  [a-z]/{//!p}' "ansible/books/${book}.yaml" | sed 's/^    //g' | grep 'default(.*)' | awk  '!x[$0]++' | sed -E -e 's/^([a-z_]*): *"\{\{ *[a-z_]* *\| *default\(([a-z].*)\) *\}\}"$/\1: "{{ \2 }}"/g' -e 's/^([a-z_]*):.*default\((.*)\).*$/\1: \2/g' | sed -e '1i---' -e "s/task_/${book}_/g" > "roles/${book}/defaults/main.yaml"
     git add "roles/${book}/defaults/main.yaml"
 
     # vars
